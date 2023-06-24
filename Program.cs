c4904e278433204eb3b260b029e0c068999a3fb6
Battousai69 <crea18@mail.ru>
2023-06-24 14:08:03 +0500

    Дз к третьему практическому занятию

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача23
+/*System.Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i <= n; i++)
+{
+    System.Console.WriteLine(i*i*i);
+}*/
+
+//Задача19
+/*int Polindrom (int num1, int num2, int num3, int num4, int num5)
+{
+    if(num1 == num5 && num2 == num4)
+    {
+        System.Console.WriteLine("Данное число является полиндроном");
+    }
+    else
+    {
+        System.Console.WriteLine("Данное число не является полиндроном");
+    }
+    return 0;
+}
+System.Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int num1 = (a/10000) % 10;
+int num2 = (a/1000) % 10;
+int num3 = (a/100) % 10;
+int num4 = (a/10) % 10;
+int num5 = a % 10;
+System.Console.WriteLine(Polindrom( num1,  num2, num3,  num4, num5));*/
+
+//Задача21
+/*double d (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+}
+System.Console.WriteLine("Введите x1");
+double x1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите x2");
+double x2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите y1");
+double y1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите y2");
+double y2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите z1");
+double z1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите z2");
+double z2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine(d(x1, y1, z1, x2, y2, z2));*/
